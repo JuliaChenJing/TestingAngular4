919e9df0e9fb102c7b58f2affdf9753b2643ed95
Julia Chen <juliachenjing@gmail.com>
2018-02-12 15:14:13 -0500

    event emmitter

--- my-section2/src/app/05-event-emitters/vote.component.spec.ts
+++ my-section2/src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,16 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  var component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upvoted', () => {
+    let flag = null;
+    component.voteChanged.subscribe(value => flag = value)
+    component.upVote();
+    expect(flag).toBe(1);
   });
 });
