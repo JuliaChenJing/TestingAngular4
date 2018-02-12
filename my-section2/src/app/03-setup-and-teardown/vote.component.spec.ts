838c9acddeb88aa267f7aa47ed7212962588ecd4
Julia Chen <juliachenjing@gmail.com>
2018-02-12 14:54:45 -0500

    before each and before all

--- my-section2/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ my-section2/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,44 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    //Arrange
+    //component = new VoteComponent();
+    console.log("---------- beforeEach")
   });
 
-  it('', () => {
+  afterEach(() => {
+    //Clean
+    console.log("----------afterEach")
+  });
+
+  beforeAll(() => {
+    //Arrange
+    component = new VoteComponent();
+    console.log("---------- beforeAll")
+  });
+
+
+  afterAll(() => {
+    //Clean
+    console.log("----------afterAll")
+  });
+
+
+  it('shoud increase totalVotes when upvoted', () => {
+    //Act
+    component.upVote();
+    //Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('shoud decrease totalVotes when downvoted', () => {
+
+    //Act
+    component.downVote();
+    //Assert
+    expect(component.totalVotes).toBe(-1);
   });
 });
