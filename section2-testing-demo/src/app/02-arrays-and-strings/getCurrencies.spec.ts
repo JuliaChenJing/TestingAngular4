3cd44f09091ec81800d81662fceacf6024cd8724
Julia Chen <juliachenjing@gmail.com>
2018-02-12 14:26:40 -0500

    demo worked

---
+++ section2-testing-demo/src/app/02-arrays-and-strings/getCurrencies.spec.ts
@@ -0,0 +1,11 @@
+import {getCurrencies} from './getCurrencies'
+import { getCiphers } from 'crypto';
+
+describe('Get Currencies', () => {
+    it('should return the suported currencies', () => {
+        const result=getCurrencies();
+        expect(result).toContain('USD');
+        expect(result).toContain('AUD');
+        expect(result).toContain('EUR');
+    })
+})
