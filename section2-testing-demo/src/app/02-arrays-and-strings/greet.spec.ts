a3bc701ba6b09df0d363d774335e57e02ee8021b
Julia Chen <juliachenjing@gmail.com>
2018-02-13 13:35:02 -0500

    updated

--- section2-testing-demo/src/app/02-arrays-and-strings/greet.spec.ts
+++
@@ -1,8 +0,0 @@
-import {greet} from './greet';
-
-describe('greet', () => {
-    it('should should include the name in the message', () => {
-        expect(greet('mosh')).toContain('mosh');
-
-    })
-})
