2257a4940967dc029ea53ad6ec7c52c24ab29556
Julia Chen <juliachenjing@gmail.com>
2018-02-13 15:50:39 -0500

    lecture 17

--- last-secion3-integration-test-demo/src/app/1-voter/voter.component.spec.ts
+++ last-secion3-integration-test-demo/src/app/1-voter/voter.component.spec.ts
@@ -1,6 +1,6 @@
 import { VoterComponent } from './voter.component';
 import { TestBed, ComponentFixture } from '@angular/core/testing'
-
+import { By } from '@angular/platform-browser';
 describe('VoterComponent', () => {
   let component: VoterComponent;
   let fixture: ComponentFixture<VoterComponent>;
@@ -15,6 +15,20 @@ describe('VoterComponent', () => {
     //fixxture.debugElement
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+    let debugElement = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = debugElement.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should high light the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+    let debugElement = fixture.debugElement.query(By.css('.glyphicon.menu.up'));
+    expect(debugElement.classes['highlighted']).toBeTruthy();
   });
 });
--- last-secion3-integration-test-demo/src/app/app.routes.ts
+++ last-secion3-integration-test-demo/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { HomeComponent } from './home/home.component';
 import { TodosComponent } from './2-todos/todos.component';
 import { UsersComponent } from './users/users.component'; 
 import { UserDetailsComponent } from './3-user-details/user-details.component';
+import {VoterComponent} from './1-voter/voter.component'
 
 export const routes = [
+  { path: 'voter', component: VoterComponent },
   { path: 'users/:id', component: UserDetailsComponent },
   { path: 'users', component: UsersComponent },
   { path: 'todos', component: TodosComponent },
